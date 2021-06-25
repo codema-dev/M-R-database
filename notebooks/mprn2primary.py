7839734de4e1e54042bdebe1a24c456eef0ea9ff
Rowan Molony <rowan.molony@codema.ie>
2021-06-25 16:55:07 +0100

    Fix incorrect filepath in mprn2primary

--- notebooks/mprn2primary.py
+++ notebooks/mprn2primary.py
@@ -6,13 +6,14 @@ outpath = pathlib.Path("../data/mprn_total_primary_energy_requirement.csv")
 total_final_consumption_to_total_primary_requirement = 1.830256781
 year = "2020"
 
+
 def drop_commas_in_numeric_columns(s: pd.Series) -> pd.Series:
     return s.str.replace(",", "")
 
 
 index_columns = ["MPRN", "SiteID", "Division"]
 columns_to_drop = ["Location", "Unit"]
-mprn_raw = pd.read_csv(path, header=34)
+mprn_raw = pd.read_csv(inpath, header=34)
 
 assert set(index_columns).issubset(mprn_raw.columns)
 assert set(columns_to_drop).issubset(mprn_raw.columns)
