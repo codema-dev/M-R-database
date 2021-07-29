9ce1aaa2f6a8100197d0957e3324c3a6434edc00
Niyati <niyatimanu@gmail.com>
2021-07-29 15:06:45 +0100

    Minor bug fixes

--- convert_mprn_to_X.py
+++ convert_mprn_to_X.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import pathlib
+import pathlib 
+from cost_table import cost_table_elec
 
 # + tags=["parameters"]
-upstream = None
-product = {"nb": "output/mprn2primary.ipynb", "data": "output/mprn_tper_2020.csv"}
+#upstream = None
+#product = {"nb": "output/mprn2primary.ipynb", "data": "output/mprn_tper_2020.csv"}
 # -
 
 inpath = pathlib.Path("data/SDCC - MPRN Consumption (2020) -V1.csv")#
@@ -34,13 +35,13 @@ mprn = (
     .apply(drop_commas_in_numeric_columns, axis="columns")
     .astype("float64")
 ) 
-mprn_recent = mprn.iloc[:, 0]
+mprn_recent = pd.DataFrame(mprn.iloc[:, 0])
 mprn_recent.columns = ['TFC(kwh)']
-
-#mprn_recent['TFC(kwh)'] = mprn_recent[0]
-#mprn_recent['TPER(kwh)'] = mprn_recent['TFC(kwh)'] * total_final_consumption_to_total_primary_requirement
-#mprn_recent['CO2(kg)'] = mprn_recent['TFC(kwh)'] * total_final_consumption_to_co2
+mprn_recent['TPER(kwh)'] = mprn_recent['TFC(kwh)'] * total_final_consumption_to_total_primary_requirement
+mprn_recent['CO2(kg)'] = mprn_recent['TFC(kwh)'] * total_final_consumption_to_co2
+mprn_recent['Cost (Euro/kwh)'] = mprn_recent['TFC(kwh)'].apply(cost_table_elec)#, axis = 'columns')
+mprn_recent['Euro'] = mprn_recent['TFC(kwh)'] * mprn_recent['Cost (Euro/kwh)']
 
 mprn_recent
 
-mprn[year].to_csv(product["data"])
+#mprn[year].to_csv(product["data"])
